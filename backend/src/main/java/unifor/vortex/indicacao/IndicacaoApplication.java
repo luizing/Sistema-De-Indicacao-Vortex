e0a15a13d5d49015b29320d735e347c98dc3667a
Luiz <luizeduardo2099@gmail.com>
2025-10-05 22:46:28 -0300

    feat: setup inicial do backend

---
+++ backend/src/main/java/unifor/vortex/indicacao/IndicacaoApplication.java
@@ -0,0 +1,13 @@
+package unifor.vortex.indicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndicacaoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/unifor/vortex/indicacao/IndicacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package unifor.vortex.indicacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndicacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
